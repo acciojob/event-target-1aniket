f57467146389470dcc423b50bbac1bbb631a6bac
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2025-01-04 22:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    const callbacks = this.listeners.get(event);
+    callbacks.add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    if (this.listeners.has(event)) {
+      const callbacks = this.listeners.get(event);
+      callbacks.delete(callback);
+      if (callbacks.size === 0) {
+        this.listeners.delete(event);
+      }
+    }
+  }
+
+  dispatchEvent(event) {
+    if (this.listeners.has(event)) {
+      const callbacks = this.listeners.get(event);
+      for (const callback of callbacks) {
+        callback();
+      }
+    }
+  }
+}
